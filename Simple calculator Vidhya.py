ac47cb88447dbd6f3c90430afd71b42d3c440112
Vidhya.E <129002240+345678jhgfxcvbnmhgf@users.noreply.github.com>
2023-12-09 13:32:35 +0530

    Add files via upload

---
+++ Simple calculator Vidhya.py
@@ -0,0 +1,32 @@
+# Simple calculator using Python
+print("Select 1 to 4:")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+
+
+operation = input()
+# Addition
+if operation == "1":
+    num1 = input("Enter first number:")
+    num2 = input("Enter second number:")
+    print("The sum is " + str(int(num1) + int(num2)))
+# subtraction    
+elif operation == "2":
+    num1 = input("Enter first number:")
+    num2 = input("Enter second number:")
+    print("The difference is " + str(int(num1) - int(num2)))
+ # Multiplication   
+elif operation == "3":
+    num1 = input("Enter first number:")
+    num2 = input("Enter second number:")
+    print("The product is " + str(int(num1) * int(num2)))
+  # Division  
+elif operation == "4":
+    num1 = input("Enter first number:")
+    num2 = input("Enter second number:")
+    print("The result is " + str(int(num1) / int(num2)))
+    
+else:
+    print("Invalid Entry")
